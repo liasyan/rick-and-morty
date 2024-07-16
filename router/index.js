39b170e0a3829e26054d56fd980c8cf585b4f69d
lia <lia.sahakyan@sceon.am>
2024-07-16 21:03:59 +0400

    fix: use post for fetching data

--- router/index.js
+++ router/index.js
@@ -4,7 +4,8 @@ const postController  = require('../controller/post');
 const getController  = require('../controller/get');
 
 module.exports = ({ insert, get }) => {
-    router.post('/insert/characters', postController(insert));
+    //insert must be post request, but for easy checking with replit I will use get
+    router.get('/insert/characters', postController(insert));
     router.get('/characters', getController(get));
 
     return router;
